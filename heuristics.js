781f56845cb7ab6795025eb36bdc62dcf69556d8
aayushker <singhaayushker@gmail.com>
2025-04-26 21:30:12 +0530

    refactor: Improve code formatting and indentation in heuristics.js for consistency

--- heuristics.js
+++ heuristics.js
@@ -1,20 +1,20 @@
 // === heuristics.js ===
 export function isSuspiciousUrl(url) {
-    try {
-        const parsed = new URL(url);
-        const hostname = parsed.hostname;
-        const badTLDs = ['.tk', '.ml', '.ga', '.cf', '.gq'];
-        const hasIP = /^\d{1,3}(\.\d{1,3}){3}$/.test(hostname);
-        const tooManyHyphens = hostname.split('-').length > 5;
-        const isHttp = parsed.protocol === 'http:';
+  try {
+    const parsed = new URL(url);
+    const hostname = parsed.hostname;
+    const badTLDs = [".tk", ".ml", ".ga", ".cf", ".gq"];
+    const hasIP = /^\d{1,3}(\.\d{1,3}){3}$/.test(hostname);
+    const tooManyHyphens = hostname.split("-").length > 5;
+    const isHttp = parsed.protocol === "http:";
 
-        return (
-            badTLDs.some(tld => hostname.endsWith(tld)) ||
-            hasIP ||
-            tooManyHyphens ||
-            isHttp
-        );
-    } catch (err) {
-        return false;
-    }
+    return (
+      badTLDs.some((tld) => hostname.endsWith(tld)) ||
+      hasIP ||
+      tooManyHyphens ||
+      isHttp
+    );
+  } catch (err) {
+    return false;
+  }
 }
