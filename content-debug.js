c2385863a315cbed38e7549896db67a76f76843d
aayushker <singhaayushker@gmail.com>
2025-04-26 21:29:26 +0530

    refactor: Update string formatting in content-debug.js for consistency

--- content-debug.js
+++ content-debug.js
@@ -1,6 +1,6 @@
 // Simple debug content script
-console.log('URL Scanner Debug: Content script loaded and running');
-document.body.style.border = '5px solid red';
+console.log("URL Scanner Debug: Content script loaded and running");
+document.body.style.border = "5px solid red";
 
 // Alert to make debugging very obvious
-alert('URL Scanner Debug: Content script is running!'); 
+alert("URL Scanner Debug: Content script is running!");
